70ccbe8f1b160835fe7deec9ca8815f2cfb0d0d9
Marianasofid <marianadelgado@colegioguanenta.edu.co>
2025-08-06 08:58:19 -0500

    actualizacion

---
+++ piedra_papel_tijera.py
@@ -0,0 +1,15 @@
+from tkinter import * 
+
+ventana_principal = Tk()
+
+ventana_principal.title("Mariana Sofia Delgado Solano")
+
+ventana_principal.geometry("800x500")
+
+ventana_principal.resizable(0,0)
+
+ventana_principal.config(bg="black")
+
+
+
+ventana_principal.mainloop()
